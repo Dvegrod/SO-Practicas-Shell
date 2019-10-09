d46f47e5906edc12dce63491373cde7361b00e48
Carlos Torres <>
2019-10-09 02:13:28 +0200

    Implemented new extra_info structure
    HIST command function now works perfectly. All cmd functions now take the same parameters. Also redid the disposeAll function, the CreateList function. Integrated the extra_info structure in the ProcessInput function and in the InsertElement functions

--- P1/p1.c
+++ P1/p1.c
@@ -10,24 +10,38 @@
 #include <errno.h>
 #define MAXLEN 256 //maximum length of strings (arrays of 256 chars)
 #define MAX_N_ARG 32 //maximum number of arguments to a shell command
+//Estructuras:
+struct node {
+  char com[MAXLEN];
+  struct node *next;
+};
+
+typedef struct extra_info {
+  struct node * lista;
+} EXINF;
+
+struct strfiledata {
+  char * inodenum,*filetype,*permissions,*hlinksnum,*user,*group,*size,*date,*name;
+};
 
 //Lista:
 
-void CreateList(struct node **plist){
-    *plist = NULL;
+struct node * CreateList(){
+    return NULL;
 }
 
-void InsertElement(struct node **plist, char element[]) {
-  struct node **last = plist;
-  while (*last != NULL) { //Looks for the last pointer
-    last = &(*last)->next;
-  };
-  *last = (struct node *) malloc(sizeof(struct node)); //Memory request for the new node
-  int i;
-  for (i = 0; (element[i] != '\0') && (i < MAXLEN); i++) //Saving command
-    (*last)->com[i] = element[i];
-  (*last)->com[i-1] = '\0'; //Substitutes the \n at the end of stored commands by \0
-  (*last)->next = NULL;
+void InsertElement(struct node ** last, char *element){
+    int i;
+    while (*last != NULL){ //'last' points to the last node
+        last = &((*last)->next); //moves one node forward
+    }
+    *last = malloc(sizeof(struct node)); //allocates memory for the new node
+    (*last)->next = NULL; //sets to null the new end of the list
+    for (i = 0; (element[i] != '\0') && (i < MAXLEN); i++)
+    {
+        (*last)->com[i] = element[i];
+    }
+    (*last)->com[i-1] = '\0'; //replaces the endline character with the end of string flag
 }
 
 int RemoveElement(struct node **plist, int position){
@@ -44,11 +58,18 @@ int RemoveElement(struct node **plist, int position){
   return 0;
 }
 
+void disposeAll(struct node ** ptolist) {
+  // while (RemoveElement(ptolist,0) == 0);
+  struct node * aux;
+  while (*ptolist != NULL)
+  {
+      aux = *ptolist;
+      *ptolist = aux->next;
+      free(aux);
+  }
+}
+
 //Shell:
-//Estructuras:
-struct strfiledata {
-  char * inodenum,*filetype,*permissions,*hlinksnum,*user,*group,*size,*date,*name;
-};
 
 //Funciones para leer los comandos de la Shell
 
@@ -56,10 +77,11 @@ void printPrompt(){
     printf("# ");
 }
 
-void readInput(char comando[], struct node* lista){
+int readInput(char comando[], struct extra_info * ex_inf){
     fgets(comando,MAXLEN,stdin);
-    if (comando[0] != '\n')
-        InsertElement(&lista, comando);
+    if (comando[0]!='\n')
+        InsertElement(&(ex_inf->lista), comando);
+    return 0;
 }
 
 int TrocearCadena(char * cadena, char * trozos[]){
@@ -74,7 +96,7 @@ int TrocearCadena(char * cadena, char * trozos[]){
 // Funciones de utilidad dadas en el enunciado:
 
 char TipoFichero (mode_t m){
-    switch (m&S_IFMT) { /*and bit a bit con los bits de formato,0170000 */
+    switch (m & S_IFMT) { /*and bit a bit con los bits de formato,0170000 */
         case S_IFSOCK: return 's'; /*socket */
         case S_IFLNK: return 'l'; /*symbolic link*/
         case S_IFREG: return '-'; /* fichero normal*/
@@ -145,7 +167,7 @@ char * ConvierteModo3 (mode_t m){
 
 // Funciones para las tareas de cada comando de la shell
 
-int autores(const char * trozos[], int ntrozos, struct extra_inf *ex_inf){
+int autores(char * trozos[], int ntrozos, struct extra_info *ex_inf){
     char *opcion = trozos[1];
     if (opcion == NULL){ //If there is no option
         printf("Carlos Torres Paz : carlos.torres\n");
@@ -165,13 +187,13 @@ int autores(const char * trozos[], int ntrozos, struct extra_inf *ex_inf){
         }
         else
         {
-            printf("%s : unrecognised command option\n",opcion);
+            printf(" %s : unrecognised command option\n",opcion);
         }
     }
     return 0;
 }
 
-int pid(const char * trozos[], int ntrozos, struct extra_inf *ex_inf){
+int pid(char * trozos[], int ntrozos, struct extra_info *ex_inf){
     char *opcion = trozos[1];
     if (opcion==NULL)
     {
@@ -184,14 +206,14 @@ int pid(const char * trozos[], int ntrozos, struct extra_inf *ex_inf){
             printf("PID of parent process: %i\n", getppid());
         }
         else{
-            printf("%s : unrecognised command option\n", opcion);
+            printf(" %s : unrecognised command option\n", opcion);
             return -1;
         }
     }
     return 0;
 }
 
-int cdir(const char * trozos[], int ntrozos, struct extra_inf *ex_inf){
+int cdir(char * trozos[], int ntrozos, struct extra_info *ex_inf){
     char *opcion = trozos[1];
     char buf[MAXLEN];
     if (opcion == NULL){
@@ -207,7 +229,7 @@ int cdir(const char * trozos[], int ntrozos, struct extra_inf *ex_inf){
     return 0;
 }
 
-int fecha(const char * trozos[], int ntrozos, struct extra_inf *ex_inf){
+int fecha(char * trozos[], int ntrozos, struct extra_info *ex_inf){
     time_t now; //time type variable
     time(&now); //writes current time to NOW variable
     struct tm *local = localtime(&now); //a structure to separate minutes, hours, day, month, year
@@ -215,7 +237,7 @@ int fecha(const char * trozos[], int ntrozos, struct extra_inf *ex_inf){
     return 0;
 }
 
-int hora(const char * trozos[], int ntrozos, struct extra_inf *ex_inf){
+int hora(char * trozos[], int ntrozos, struct extra_info *ex_inf){
     time_t now; //time type variable
     time(&now); //writes current time to NOW variable
     struct tm *local = localtime(&now); //a structure to separate minutes, hours, day, month, year
@@ -223,41 +245,38 @@ int hora(const char * trozos[], int ntrozos, struct extra_inf *ex_inf){
     return 0;
 }
 
-void disposeAll(struct node **plist) {
-  while (RemoveElement(plist,0) == 0);
-}
-
-int hist(const char * trozos[], int ntrozos, struct extra_inf *ex_inf){
-    char *opcion = trozos[1];
-    if (opcion != NULL) { //if there IS an option
-        if (!strcmp(opcion,"-c")){ //if that option is "-c"
-        disposeAll(&lista);
+int hist(char * trozos[], int ntrozos, struct extra_info *ex_inf){
+  char *opcion = trozos[1];
+  if (opcion != NULL) { //if there IS an option
+    if (!strcmp(opcion,"-c")){ //if that option is "-c"
+        disposeAll(&(ex_inf->lista));
         printf("Cleared command history\n");
     }
-    else{
-        printf("%s : unrecognised command option\n",opcion);
+    else
+    {
+        printf(" %s : unrecognised command option\n",opcion);
         return -1;
     }
+  }
+  else { //if there is NO option
+    struct node *pointer = (ex_inf->lista);
+    for (int i = 1; pointer != NULL; i++) {
+      printf("%d > %s\n",i,pointer->com);
+      pointer = pointer->next;
     }
-    else {
-        struct node * pointer = lista;
-        for (int i = 1; pointer != NULL; i++) {
-            printf("%d > %s\n",i,pointer->com);
-            pointer = pointer->next;
-        };
-    }
+  }
+  return 0;
+}
+
+int crear(char * trozos[], int ntrozos, struct extra_info *ex_inf){
     return 0;
 }
 
-int crear(const char * trozos[], int ntrozos, struct extra_inf *ex_inf){
-    
+int borrar(char * trozos[], int ntrozos, struct extra_info *ex_inf){
+    return 0;
 }
 
-int borrar(const char * trozos[], int ntrozos, struct extra_inf *ex_inf){
-
-}
-
-struct strfiledata getInfo(const char *path){
+struct strfiledata getInfo(char *path){
   struct strfiledata strfinfo;
   struct stat *finfo = NULL;
   if (lstat(path,finfo) < 0) printf("%s",strerror(errno)); //Syscall
@@ -285,31 +304,29 @@ struct strfiledata getInfo(const char *path){
   return strfinfo;
 }
 
-int info(const char * trozos[], int ntrozos, struct extra_inf *ex_inf){
-
+int info(char * trozos[], int ntrozos, struct extra_info *ex_inf){
+    return 0;
 }
 
-int listar(const char * trozos[], int ntrozos, struct extra_inf *ex_inf){
-
+int listar(char * trozos[], int ntrozos, struct extra_info *ex_inf){
+    return 0;
 }
 
-int salir(const char *trozos[], int ntrozos, struct extra_inf *ex_inf){
+int salir(char *trozos[], int ntrozos, struct extra_info *ex_inf){
     return 1;
 }
 
 //Función para decidir qué comando se va a ejecutar
 
-int processInput(char comando[], struct extra_inf *ex_inf){
-
-
+int processInput(char comando[], struct extra_info *ex_inf){
     char *trozos[MAX_N_ARG];
     int ntrozos = TrocearCadena(comando,trozos);
-    int i =0;
-    int return_value  =0;
+    int i = 0;
+    int return_value = 0;
 
-    struct{
+    struct {
         char * cmd_name;
-        int (* cmd_fun) (const char * trozos[], int ntrozos, struct extra_inf *ex_inf)
+        int (* cmd_fun) (char * trozos[], int ntrozos, struct extra_info *ex_inf);
     } cmds[] = {
         {"autores", autores},
         {"pid", pid},
@@ -326,6 +343,10 @@ int processInput(char comando[], struct extra_inf *ex_inf){
         {"exit", salir},
         {NULL, NULL}
     };
+
+    if (trozos[0]==NULL){ //in case nothing is entered
+        return 0;
+    }
     
     for (i = 0; cmds[i].cmd_name != NULL; i++)
     {
@@ -344,23 +365,16 @@ int processInput(char comando[], struct extra_inf *ex_inf){
 int main(int argc, char const *argv[]){
     char comando[MAXLEN];
 
-    struct node {
-        char com[MAXLEN];
-        struct node *next;
-    };
+    struct extra_info * ex_inf;
+    ex_inf = (struct extra_info *) malloc(sizeof(struct extra_info));
 
-    struct extra_inf {
-        struct node * lista;
-    };
-
-    struct extra_inf * ex_inf;
-    ex_inf = (struct extra_inf *) malloc(sizeof(struct extra_inf));
+    ex_inf->lista = CreateList();
 
     do{
         printPrompt();
-        readInput(comando, ex_inf->lista);
-    } while(processInput(comando, &salir) != 1);
+        readInput(comando, ex_inf);
+    } while(processInput(comando, ex_inf) != 1);
 
-    disposeAll(&(ex_inf-> lista));
+    disposeAll(&(ex_inf->lista));
     return 0;
 }
