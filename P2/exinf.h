08470322c058e0bc3fdda78b38ec038e0977a057
Carlos Torres <>
2019-10-28 23:10:34 +0100

    Asignar function

--- P2/exinf.h
+++ P2/exinf.h
@@ -14,6 +14,9 @@
   #include <time.h>
   #include <sys/types.h>
   #include <sys/stat.h>
+  #include <sys/mman.h>
+  #include <sys/ipc.h>
+  #include <sys/shm.h>
   #include <dirent.h>
   #include <fcntl.h>
   #include <unistd.h>
--- P2/p2commands.c
+++ P2/p2commands.c
@@ -58,5 +58,97 @@ int asignar_malloc(char const * trozos[], int ntrozos, struct extra_info *ex_inf
 }
 
 int asignar_mmap(char const * trozos[], int ntrozos, struct extra_info *ex_inf){
+    unsigned int permisos_open = 0;
+    unsigned int permisos_mmap = 0;
+    struct stat * statbuf;
+    int i = 0;
+    int fd = 0;
+    void *file;
     char *path = trozos[2];
+    if (path==NULL){
+        //mostrar lista de direcciones de mmap
+        return 0;
+    }
+    //lee los permisos pasados por parámetro
+    if (trozos[3]==NULL){
+        permisos_open = S_IRWXU;
+    }
+    else{
+        while (trozos[3][i]!='\0')
+        {
+            if (trozos[3][i]=='r'){ (permisos_open |= S_IRUSR); (permisos_mmap |= PROT_READ);};
+            if (trozos[3][i]=='w'){ (permisos_open |= S_IWUSR); (permisos_mmap |= PROT_WRITE);};
+            if (trozos[3][i]=='x'){ (permisos_open |= S_IXUSR); (permisos_mmap |= PROT_EXEC);};
+            i++;
+        }
+    }
+    errno = 0;
+    fd = open(path,0,permisos_open);
+    if (fd==-1){
+        perror(strerror(errno));
+        return -1;
+    }
+    errno = 0;
+    if (fstat(fd,statbuf)==-1){
+        free(statbuf);
+        perror(strerror(errno));
+        return -1;
+    }
+    errno = 0;
+    file = mmap(NULL,statbuf->st_size,permisos_mmap,MAP_SHARED,fd,0);
+    if (file == MAP_FAILED){
+        free(statbuf);
+        perror(strerrro(errno));
+        return -1;
+    }
+    printf("File %s mapped at %p",path,file);
+    //almacenar entrada en la lista de mmap
+    return 0;
+}
+
+int asignar_crear_shared(char const * trozos[], int ntrozos, struct extra_info *ex_inf){
+    int key = 0,size = 0,shared_id = 0;
+    void *shm_ptr;
+    if ((trozos[2]==NULL) || (trozos[3]==NULL)){ //no key or size are specified
+        //mostrar lista de direcciones de áreas de memoria compartidas
+    }
+    key = atoi(trozos[2]);
+    size = atoi(trozos[3]);
+    errno = 0;
+    shared_id = shmget(key,size,IPC_CREAT|IPC_EXCL);
+    if (shared_id==-1){ //if shmget fails
+        perror(strerror(errno));
+        return -1;
+    }
+    errno = 0;
+    shm_ptr = shmat(shared_id,NULL,0);
+    if (shm_ptr == MAP_FAILED){
+        perror(strerror(errno));
+        return -1;
+    }
+    printf("Allocated shared memory (key %d) at %p",key,shm_ptr);
+    //añadir a la lista de direcciones de áreas de memoria compartidas
+}
+
+int asignar_shared(char const * trozos[], int ntrozos, struct extra_info * ex_inf){
+    int key = 0, shared_id = 0;
+    void *shm_ptr;
+    if (trozos[2]==NULL){ //no key is specified
+        //mostrar lista de direcciones de áreas de memoria compartidas
+    }
+    key = atoi(trozos[2]);
+    errno = 0;
+    shared_id = shmget(key,0,0);
+    if (shared_id == -1){
+        perror(strerror(errno));
+        return -1;
+    }
+    shm_ptr = shmat(shared_id,NULL,0);
+    if (shm_ptr == MAP_FAILED){
+        perror(strerror(errno));
+        return -1;
+    }
+    printf("Allocated shared memory (key %d) at %p",key,shm_ptr);
+    //añadir a la lista de direcciones de áreas de memoria compartidas
+    return 0;
 }
